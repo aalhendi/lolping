6e6bee92d5d656dad0a3eeccaa3e2ed97f8150d5
Abdulrazaq Alhendi <abdulrazaq.alhendi@gmail.com>
2023-06-26 08:46:35 +0300

    fix: x-axis (time) units and overall plot style

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use plotters::prelude::*;
-use std::fs;
+use std::{error::Error, fs};
 
 // struct Entry {
 //     time: i32,
@@ -28,49 +28,86 @@ use std::fs;
 //     reconnect: i32,
 // }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+// TODO: User selects features (command line args?)
+const IS_GRID_ENABLED: bool = false;
+const IS_POINTS_ENABLED: bool = true;
+const IS_LINE_ENABLED: bool = true;
+
+fn main() -> Result<(), Box<dyn Error>> {
     let file_path = "2023-03-04T16-39-53/2023-03-04T16-39-53_netlog.txt";
-    let contents = fs::read_to_string(file_path).expect("lole");
+    let contents = fs::read_to_string(file_path).expect("Unable to read file contents");
 
     let mut data = Vec::new();
-    for line in contents.lines().skip(28) {
-        let mut split = line.split(",");
-        let cur_time = split.nth(0);
-        let cur_ping = split.nth(7);
-        if cur_ping.is_some() && cur_time.is_some() {
-            //ping.push(cur_ping.unwrap());
-            //time.push(cur_time.unwrap());
-            data.push((cur_time.unwrap(), cur_ping.unwrap()));
-        }
-        //println!("{line}");
-    }
+    let mut max_ping = i32::MIN; // Initialize maximum to the minimum value of i32
 
-    //println!("{ping:?}");
-    //println!("{time:?}");
+    for line in contents.lines().skip(28) {
+        let mut split = line.split(',');
+        if let (Some(cur_time), Some(cur_ping)) = (split.next(), split.nth(7)) {
+            let cur_time =
+                cur_time.parse::<f64>().expect("Could not parse timestamp") / 1000.0 / 60.0;
+            let cur_ping = cur_ping.parse::<i32>().expect("Could not parse ping");
+
+            // Keep track of maximum
+            if cur_ping > max_ping {
+                max_ping = cur_ping;
+            }
+
+            data.push((cur_time, cur_ping));
+        }
+    }
 
     let root = BitMapBackend::new("./0.png", (640, 480)).into_drawing_area();
     root.fill(&WHITE)?;
+
+    let max_time = data.last().expect("Could not obtain max_time").0 * 1.05;
+    let next_fifty = ((max_ping as f64 / 50.0).ceil() * 50.0) as i32;
+
     let mut chart = ChartBuilder::on(&root)
-        .caption("ping(ms) vs time", ("sans-serif", 50).into_font())
+        .caption("Ping vs Time", ("sans-serif", 32).into_font())
         .margin(5)
-        .x_label_area_size(30)
-        .y_label_area_size(30)
-        .build_cartesian_2d(10000i32..11000i32, 0i32..200i32)?;
+        .x_label_area_size(40)
+        .y_label_area_size(55)
+        .build_cartesian_2d(0f64..max_time, 0i32..next_fifty)?;
 
-    chart.configure_mesh().draw()?;
+    // TODO: Clean up grid conditional implementation
+    if IS_GRID_ENABLED {
+        chart
+            .configure_mesh()
+            .x_desc("Time (minutes)")
+            .y_desc("Ping (ms)")
+            .axis_desc_style(("sans-serif", 15))
+            .x_label_style(("sans-serif", 15))
+            .y_label_style(("sans-serif", 15));
+    } else {
+        chart
+            .configure_mesh()
+            .x_desc("Time (minutes)")
+            .y_desc("Ping (ms)")
+            .axis_desc_style(("sans-serif", 15))
+            .x_label_style(("sans-serif", 15))
+            .y_label_style(("sans-serif", 15))
+            .disable_mesh()
+            .draw()?;
+    }
 
-    chart
-        .draw_series(LineSeries::new(
-            data.iter().map(|(t, p)| (t.parse::<i32>().unwrap(), p.parse::<i32>().unwrap())),
-            &RED,
-        ))?
-        .label("y = x^2")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+    if IS_LINE_ENABLED {
+        chart
+            .draw_series(LineSeries::new(data.iter().map(|(t, p)| (*t, *p)), &RED))?
+            .label("Ping")
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], RED));
+    }
+
+    if IS_POINTS_ENABLED {
+        chart.draw_series(
+            data.iter()
+                .map(|(t, p)| Circle::new((*t, *p), 2, RED.filled())),
+        )?;
+    }
 
     chart
         .configure_series_labels()
-        .background_style(&WHITE.mix(0.8))
-        .border_style(&BLACK)
+        .background_style(WHITE.mix(0.8))
+        .border_style(BLACK)
         .draw()?;
 
     root.present()?;
